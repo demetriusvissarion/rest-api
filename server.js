7e95ead28d470fef3e7d88a60bdeb8b9ade1ab1f
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-03-05 20:36:08 +0000

    server setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/subscribers", { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+
+app.listen(3000, () => console.log("Server Started"));
